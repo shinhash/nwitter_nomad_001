c5c464ce83ebf4b12a703e2ff243ca3815dffde3
DESKTOP-QJDHSQD\ha-seung <shinhash123@gmail.com>
2024-02-19 22:43:50 +0900

    20240219 commit fix souce layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -56,14 +56,14 @@ function Layout(){
     return (
         <Wrapper>
             <Menu>
-                <Link to="/">
+                <Link to="/profile">
                     <MenuItem>
                     <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
                     </svg>
                     </MenuItem>
                 </Link>
-                <Link to="/profile">
+                <Link to="/home">
                     <MenuItem>
                     <svg  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path clipRule="evenodd" fillRule="evenodd" d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z" />
